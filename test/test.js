098095e095cbe7c6a5245dfcdaf340a8da611c91
gemsvido <gemsvido@gmail.com>
2022-05-04 10:57:01 -0700

    SoonTM

--- test/test.js
+++ test/test.js
@@ -1,7 +1,57 @@
-const fetch = require("node-fetch");
+try {
 
-(async () => {
-    const status = await (await fetch("https://api.prodigygame.com/game-api/status")).json();
-    const gameMin = await (await fetch(`${status.data.gameCodePath}game.min.js`)).text();
-    console.log(gameMin.length);
-})();
+
+    const fetch = require("node-fetch");
+
+
+
+
+
+
+
+
+    let lastGameStatus = null;
+
+    const getGameStatus = async () => {
+    	if (lastGameStatus) return lastGameStatus;
+    	try {
+    		const json = (await (await fetch("https://math.prodigygame.com/play?launcher=true")).text()).match(
+    			/(?<=gameStatusDataStr = ').+(?=')/
+    		);
+    		if (!json?.length) return null;
+    		return JSON.parse(json[0]);
+    	} catch (e) {
+    		console.warn(`An error occurred while obtaining the game status.\n${e}`);
+    		return null;
+    	}
+    };
+
+    const gs = getGameStatus();
+
+	const version = gs.gameClientVersion;
+
+
+
+
+
+
+
+
+
+
+    (async () => {
+        const status = await (await fetch("https://api.prodigygame.com/game-api/status")).json();
+
+
+        const gameMinURL = `https://code.prodigygame.com/code/${version}/game.min.js?v=${version}`;
+
+        const gameMin = await (await fetch(gameMinURL)).text();
+        console.log("Game Min Version: " + version);
+        console.log("GameMinLength: " + gameMin.length);
+        console.log("GameMinURL: " + gameMinURL);
+    })();
+
+
+} catch (e) {
+    console.error(e);
+}
